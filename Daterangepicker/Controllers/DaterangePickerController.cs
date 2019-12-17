717aa66629cbe243e56e289a0cb6ac36681782e8
OliverQueen <tung0963242091@gmail.com>
2019-12-17 22:09:44 +0700

    date

---
+++ Daterangepicker/Controllers/DaterangePickerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Daterangepicker.Controllers
+{
+    public class DaterangePickerController : Controller
+    {
+        // GET: DaterangePicker
+        public ActionResult Index(string start, string end)
+
+        {
+
+            var startTime = DateTime.Now;
+
+            startTime = startTime.AddYears(-1);
+
+            try
+
+            {
+
+                startTime = DateTime.Parse(start.ToString());
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e);
+
+            }
+
+
+
+            var endTime = DateTime.Now;
+
+            try
+
+            {
+
+                endTime = DateTime.Parse(end.ToString());
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e);
+
+            }
+
+            ViewBag.Start = startTime;
+
+            ViewBag.End = endTime;
+
+            return View();
+
+        }
+
+    }
+}
---
+++ Daterangepicker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Daterangepicker.MvcApplication" Language="C#" %>
